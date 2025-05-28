30f629057517138e4ec0986940685bb63807b286
razi.uddin <razi.uddin@sephora.com>
2025-05-28 16:08:00 +0530

    Variables code changes (Day1)

--- src/test/java/JavaLearn/JavaTestFile.java
+++ src/test/java/JavaLearn/JavaTestFile.java
@@ -1,4 +1,21 @@
 package JavaLearn;
 
 public class JavaTestFile {
+    int amount;
+    static int charges = 500;
+
+    public JavaTestFile() {
+        this.amount = 100;
+    }
+
+    public int newMethod() {
+        int termfee = 50;
+        int sec = 100;
+
+        int totalfee = termfee + sec;
+        return totalfee;
+
+    }
+
 }
+
---
+++ src/test/java/JavaLearn/TestClass.java
@@ -0,0 +1,13 @@
+package JavaLearn;
+
+public class TestClass {
+    public static void main(String[] args) {
+        JavaTestFile javaTestFile = new JavaTestFile();
+//        System.out.println("Current value is " + javaTestFile.amount);
+//        System.out.println("Total fee is " + javaTestFile.newMethod());
+
+//        javaTestFile.newMethod();
+        System.out.println("Total charges is " + JavaTestFile.charges);
+
+    }
+}
